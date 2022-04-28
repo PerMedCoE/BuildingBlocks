ba060967350837addfa36309abc4c851f3db7de2
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-04-28 09:17:47 +0200

    Use container version of core tools

--- MaBoSS/src/MaBoSS_BB/assets/MaBoSS_analysis/1-4_epistasis_Stew.sh
+++ MaBoSS/src/MaBoSS_BB/assets/MaBoSS_analysis/1-4_epistasis_Stew.sh
@@ -2,16 +2,18 @@
 projectname=$1
 other_args=$2
 
-java -Xmx6000M -cp './BiNoM.jar' fr.curie.BiNoM.pathways.MaBoSS.MaBoSSConfigurationFile -single ${other_args} -c ./${projectname}.cfg -b ./${projectname}.bnd
+java -Xmx6000M -cp '/usr/local/jar/BiNoM.jar' fr.curie.BiNoM.pathways.MaBoSS.MaBoSSConfigurationFile -single ${other_args} -c ./${projectname}.cfg -b ./${projectname}.bnd
 cp ./${projectname}.bnd ./${projectname}_mutants/
 cd ${projectname}_mutants/
-# sed -i 's:../MaBoSS:MaBoSS:' run.sh
+sed -i 's:../MaBoSS:MaBoSS:' run.sh
 chmod 755 run.sh
 echo "running MaBoSS instances"
 ./run.sh
 cd ..
-java -Xmx6000M -cp './BiNoM.jar:./VDAOEngine.jar' fr.curie.BiNoM.pathways.MaBoSS.MaBoSSProbTrajFile -maketable -folder ${projectname}_mutants/ -prefix ${projectname} -out ${projectname}.xls
-java -Xmx6000M -cp './BiNoM.jar:./VDAOEngine.jar'  fr.curie.BiNoM.pathways.MaBoSS.MaBoSSProbTrajFile -normtable -table ${projectname}.xls
+echo java -Xmx6000M -cp '/usr/local/jar/BiNoM.jar:/usr/local/jar/VDAOEngine.jar' fr.curie.BiNoM.pathways.MaBoSS.MaBoSSProbTrajFile -maketable -folder ${projectname}_mutants/ -prefix ${projectname} -out ${projectname}.xls
+echo java -Xmx6000M -cp '/usr/local/jar/BiNoM.jar:/usr/local/jar/VDAOEngine.jar'  fr.curie.BiNoM.pathways.MaBoSS.MaBoSSProbTrajFile -normtable -table ${projectname}.xls
+java -Xmx6000M -cp '/usr/local/jar/BiNoM.jar:/usr/local/jar/VDAOEngine.jar' fr.curie.BiNoM.pathways.MaBoSS.MaBoSSProbTrajFile -maketable -folder ${projectname}_mutants/ -prefix ${projectname} -out ${projectname}.xls
+java -Xmx6000M -cp '/usr/local/jar/BiNoM.jar:/usr/local/jar/VDAOEngine.jar'  fr.curie.BiNoM.pathways.MaBoSS.MaBoSSProbTrajFile -normtable -table ${projectname}.xls
 mkdir "${projectname}_epistasis"
 mv ${projectname}_m* ${projectname}_n* ./${projectname}_epistasis/
 mv ${projectname}.xls ./${projectname}_epistasis/
