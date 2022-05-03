fa811bc7dd2fe27fe8e531a0fe246bbab0480e95
fconejer <francisco.conejero@bsc.es>
2022-05-03 15:40:52 +0200

    Cleaned new BB singularity files.

---
+++ Resources/images/build_all.sh
@@ -0,0 +1,4 @@
+sudo /usr/local/bin/singularity build MaBoSS.sif MaBoSS.singularity
+sudo /usr/local/bin/singularity build PhysiCell-COVID19.sif PhysiCell-COVID19.singularity
+# sudo /usr/local/bin/singularity build single_cell.sif single_cell.singularity
+# sudo /usr/local/bin/singularity build meta_analysis.sif meta_analysis.singularity
