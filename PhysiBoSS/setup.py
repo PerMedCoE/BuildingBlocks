8cd1583cac9fb14d0fa8e4af98e06a133a91f634
fconejer <francisco.conejero@bsc.es>
2022-12-19 15:52:26 +0100

    Updated permedcoe package requirement to 0.0.6

--- PhysiBoSS/setup.py
+++ PhysiBoSS/setup.py
@@ -36,7 +36,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.6, <4",
-    install_requires=["permedcoe>=0.0.1"],
+    install_requires=["permedcoe>=0.0.6"],
     extras_require={
         "dev": ["check-manifest"],
     },
