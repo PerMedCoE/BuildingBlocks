2f006e36d7f48f6d5541dd203b6de3a1c6650d2e
fconejer <francisco.conejero@bsc.es>
2023-11-06 13:13:53 +0100

    Minor typo in personalize_patient BB.

--- personalize_patient/src/personalize_patient_BB/definitions.py
+++ personalize_patient/src/personalize_patient_BB/definitions.py
@@ -8,6 +8,6 @@ BB_SOURCE_PATH=os.path.dirname(os.path.abspath(__file__))
 # Update the following lines:
 #  - Assets folder within the Building Block
 ASSETS_PATH = os.path.join(BB_SOURCE_PATH, "assets")
-#  - Container definition for Meta Analysis Building Block
+#  - Container definition for Personalize Patient Building Block
 CONTAINER_NAME = "physicell_covid19.sif"
 CONTAINER = os.path.join(CONTAINER_PATH, CONTAINER_NAME)
