15b522b0e57d09e1b1a7a52a6007f63f37a3afd0
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-11-24 11:00:40 +0200

    updates

--- Resources/oci-containers/single_cell/install.R
+++ Resources/oci-containers/single_cell/install.R
@@ -1,15 +1,32 @@
-install.packages('pacman', repos='http://cran.us.r-project.org')
-install.packages('BiocManager', repos='http://cran.us.r-project.org')
-install.packages('usethis')
-install.packages('devtools')
-devtools::install_version("RcppAnnoy", version = "0.0.16", repos="http://cran.us.r-project.org")
-BiocManager::install("BiocNeighbors")
-devtools::install_version("RcppAnnoy", version = "0.0.18", repos="http://cran.us.r-project.org")
-BiocManager::install("SingleR")
-BiocManager::install("limma")
-BiocManager::install("SingleCellExperiment")
-devtools::install_version(package = 'rsvd', version = package_version('1.0.2'), repos = 'http://cran.us.r-project.org')
-devtools::install_version(package = 'Seurat', version = package_version('3.2.3'), repos = 'http://cran.us.r-project.org')
-install.packages('https://cran.r-project.org/src/contrib/Archive/spatstat/spatstat_1.64-1.tar.gz', repos=NULL,type='source', INSTALL_opts = '--no-lock')
-devtools::install_version(package = 'Seurat', version = package_version('3.2.3'), repos = 'http://cran.us.r-project.org',dependencies = F)
-list.p=c("dplyr","Matrix","future","pheatmap","ggplot2","optparse","hdf5r");pacman::p_load(list.p, character.only = TRUE)
+# Directory for installing packages
+libpath <- .libPaths()[1]
+
+# Install devtools and load it
+install.packages("devtools", dependencies = TRUE, lib = libpath)
+library("devtools")
+
+# Install Bioconductor
+BiocManager::install(lib = libpath, ask = FALSE)
+
+# Install other packages
+install.packages("pacman", dependencies = TRUE, lib = libpath)
+install.packages("usethis", dependencies = TRUE, lib = libpath)
+install_version("RcppAnnoy", version = "0.0.16", dependencies = TRUE, lib = libpath)
+BiocManager::install("BiocNeighbors", lib = libpath, update = FALSE)
+install_version("RcppAnnoy", version = "0.0.18", dependencies = TRUE, lib = libpath)
+BiocManager::install("SingleR", lib = libpath, update = FALSE)
+BiocManager::install("limma", lib = libpath, update = FALSE)
+BiocManager::install("SingleCellExperiment", lib = libpath, update = FALSE)
+
+install_version(package = "rsvd", version = package_version("1.0.2"), dependencies = TRUE, lib = libpath)
+install_version(package = "Seurat", version = package_version("3.2.3"), dependencies = TRUE, lib = libpath)
+install.packages("https://cran.r-project.org/src/contrib/Archive/spatstat/spatstat_1.64-1.tar.gz",
+                 repos=NULL, type="source", INSTALL_opts = "--no-lock", dependencies = TRUE, lib = libpath)
+
+install.packages("dplyr", dependencies = TRUE, lib = libpath)
+install.packages("Matrix", dependencies = TRUE, lib = libpath)
+install.packages("future", dependencies = TRUE, lib = libpath)
+install.packages("pheatmap", dependencies = TRUE, lib = libpath)
+install.packages("ggplot2", dependencies = TRUE, lib = libpath)
+install.packages("optparse", dependencies = TRUE, lib = libpath)
+install.packages("hdf5r", dependencies = TRUE, lib = libpath)
