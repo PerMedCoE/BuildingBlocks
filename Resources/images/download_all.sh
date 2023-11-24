fdfb0a3503aa33393691e97cf1ad5af43c65f19a
fconejer <francisco.conejero@bsc.es>
2023-11-24 10:51:21 +0100

    Added download all images at once script.

--- Resources/images/download_all.sh
+++ Resources/images/download_all.sh
@@ -1,12 +1,12 @@
-apptainer pull carnivalpy.sif docker://ghcr.io/jaantollander/carnivalpy:latest
-apptainer pull maboss.sif docker://ghcr.io/jaantollander/maboss:latest
-apptainer pull meta_analysis.sif docker://ghcr.io/jaantollander/meta_analysis:latest
-apptainer pull print_results.sif docker://ghcr.io/jaantollander/print_results:latest
-apptainer pull single_cell.sif docker://ghcr.io/jaantollander/single_cell:latest
-apptainer pull ml_jax.sif docker://ghcr.io/jaantollander/ml_jax:latest
-apptainer pull toolset.sif docker://ghcr.io/jaantollander/toolset:latest
-# # Still not uploaded to https://github.com/jaantollander?tab=packages
-# apptainer pull cobrexa.jl_latest.sif docker://ghcr.io/jaantollander/cobrexa.jl_latest:latest
-# apptainer pull fromspeciestomabossmodel.sif docker://ghcr.io/jaantollander/fromspeciestomabossmodel:latest
-# apptainer pull maboss_sensitivity.sif docker://ghcr.io/jaantollander/maboss_sensitivity:latest
-# apptainer pull physicell-covid19.sif docker://ghcr.io/jaantollander/physicell-covid19:latest
+apptainer pull from_species_to_maboss_model.sif docker://ghcr.io/permedcoe/from_species_to_maboss_model:latest
+apptainer pull maboss.sif docker://ghcr.io/permedcoe/maboss:latest
+apptainer pull carnivalpy.sif docker://ghcr.io/permedcoe/carnivalpy:latest
+apptainer pull meta_analysis.sif docker://ghcr.io/permedcoe/meta_analysis:latest
+apptainer pull physicell_covid19.sif docker://ghcr.io/permedcoe/physicell_covid19:latest
+apptainer pull single_cell.sif docker://ghcr.io/permedcoe/single_cell:latest
+apptainer pull print_results.sif docker://ghcr.io/permedcoe/print_results:latest
+apptainer pull toolset.sif docker://ghcr.io/permedcoe/toolset:latest
+apptainer pull ml_jax.sif docker://ghcr.io/permedcoe/ml_jax:latest
+apptainer pull physicell_invasion.sif docker://ghcr.io/permedcoe/physicell_invasion:latest
+apptainer pull invasion_analysis.sif docker://ghcr.io/permedcoe/invasion_analysis:latest
+
